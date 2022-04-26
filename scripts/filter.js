ec535263cc3bba3f4330bd7aee0ceec1b963d71e
Vinit Raj <vinit.raj@groww.in>
2022-04-26 20:54:23 +0530

    filter updated

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,41 +1,34 @@
 function isMyType() {
-  // assuming that person drinks and smokes as well
-  let isSmoker = true,
-    isDrinker = true,
-    checkedMyType = false;
+  // assuming that person don't drink or smoke, cause if they havent answered
+  // to that, we shall leave that to GOD
+  let isSmoker = false,
+    isDrinker = false,
+    smokerType = ["Socially", "Regularly"],
+    drinkerType = ["Socially", "Regularly", "Frequently", "Sober"];
+
   // gets the attribute of the user from there bio which they have written
   stories = document.getElementsByClassName(
     "encounters-story-about__badges"
   )[0];
 
-  for (let i = 0; i < stories.length; i++) {
+  for (let i = 0; i < stories.childNodes.length; i++) {
     // to find the type of attribute this is a hack
-    imgSrc = stories[i].childNodes[0].childNodes[0].childNodes[0].src;
+    imgSrc =
+      stories.childNodes[i].childNodes[0].childNodes[0].childNodes[0].src;
     answerForAttr =
-      stories[i].childNodes[0].childNodes[1].childNodes[0].innerText;
+      stories.childNodes[i].childNodes[0].childNodes[1].childNodes[0].innerText;
 
     // checks if person added smoking attr and it should be never only
-    if (imgSrc.includes("smoking") && answerForAttr == "Never") {
-      isSmoker = false;
-      // flag to check if this has been checked
-      // as if someone has not added these attribute will pass the check
-      // as i don't want to maximise the chances. lets leave something for humans as well ;)
-      checkedMyType = true;
+    if (imgSrc.includes("smoking") && smokerType.includes(answerForAttr)) {
+      isSmoker = true;
     }
     // checks if person added drinking attr and it should be never only
-    if (imgSrc.includes("drinking") && answerForAttr == "Never") {
-      isDrinker = false;
-      checkedMyType = true;
+    if (imgSrc.includes("drinking") && drinkerType.includes(answerForAttr)) {
+      isDrinker = true;
     }
   }
 
-  // if my type has been checked then person
-  // should be non-smoker and drinker
-  if (checkedMyType) {
-    return !(isDrinker && isSmoker);
-  }
-  // else just leave to the GOD and hope for a match
-  return true;
+  return !(isDrinker && isSmoker);
 }
 
 module.exports = {
